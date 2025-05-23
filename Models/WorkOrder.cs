cb994c34c395a1910d0cca846d83b7280809c7da
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:28:01 +0200

    added workOrder model

---
+++ Models/WorkOrder.cs
@@ -0,0 +1,31 @@
+﻿namespace GaragePRO.Models;
+
+public class WorkOrder
+{
+    public int Id { get; set; }
+
+    public int VehicleId { get; set; }
+    public Vehicle Vehicle { get; set; }
+
+    public int MechanicId { get; set; }
+    public Mechanic Mechanic { get; set; }
+
+    public DateTime DateIn { get; set; }
+    public DateTime? DateOut { get; set; }
+    public string Notes { get; set; }
+
+    public WorkOrderStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<ServiceDetail> ServiceDetails { get; set; }
+    public ICollection<PartUsed> PartsUsed { get; set; }
+    public Invoice Invoice { get; set; }
+}
+
+public enum WorkOrderStatus
+{
+    Scheduled,
+    InProgress,
+    Completed,
+    Cancelled
+}
