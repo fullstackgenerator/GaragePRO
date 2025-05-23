73f3a5e01ee271ac8fc5bd3577af9577eacdf9f0
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:24:11 +0200

    updated customer model, added vehicle model

---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace GaragePRO.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public int PostalCode { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+
+    public ICollection<Vehicle> Vehicles { get; set; }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿namespace GaragePRO.Models;
+
+public class Vehicle
+{
+    public int Id { get; set; }
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public string VIN { get; set; }  // needs to be unique
+    public int Mileage { get; set; }
+
+    public ICollection<WorkOrder> WorkOrders { get; set; }
+}
