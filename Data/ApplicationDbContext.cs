f4f1425df2f41f3335b7c9f297c4d335e73ba1b3
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:44:17 +0200

    updated dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GaragePRO.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GaragePRO.Data;
@@ -8,5 +9,23 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Mechanic> Mechanics { get; set; }
+    public DbSet<WorkOrder> WorkOrders { get; set; }
+    public DbSet<ServiceDetail> ServiceDetails { get; set; }
+    public DbSet<PartCatalog> PartCatalogs { get; set; }
+    public DbSet<PartUsed> PartUsed { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Vehicle>()
+            .HasIndex(v => v.VIN)
+            .IsUnique();
     }
 }
