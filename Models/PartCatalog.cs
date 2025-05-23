acfd2a03dfa4b6117ff562b8ee9f3d92a714e58a
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:30:03 +0200

    added partCatalog model

---
+++ Models/PartCatalog.cs
@@ -0,0 +1,11 @@
+﻿namespace GaragePRO.Models;
+
+public class PartCatalog
+{
+    public int Id { get; set; }
+    public string PartName { get; set; }
+    public string PartNumber { get; set; }
+    public decimal PartPrice { get; set; }
+
+    public ICollection<PartUsed> PartsUsed { get; set; }
+}
