af7b1a5edf57f336cacdfb0501fa4f21d1872b8b
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:30:39 +0200

    added partUsed model

---
+++ Models/PartUsed.cs
@@ -0,0 +1,15 @@
+﻿namespace GaragePRO.Models;
+
+public class PartUsed
+{
+    public int Id { get; set; }
+
+    public int WorkOrderId { get; set; }
+    public WorkOrder WorkOrder { get; set; }
+
+    public int PartCatalogId { get; set; }
+    public PartCatalog PartCatalog { get; set; }
+
+    public int Quantity { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
