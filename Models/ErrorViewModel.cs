44b2e22147b5149c58e3f783ad62a208e2b6979a
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:17:47 +0200

    added customer model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GaragePRO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/customer.cs
@@ -0,0 +1,12 @@
+﻿namespace GaragePRO.Models;
+
+public class customer
+{
+    int id { get; set; }
+    string fullName { get; set; }
+    string address { get; set; }
+    string city { get; set; }
+    string postalCode { get; set; }
+    string phone { get; set; }
+    string email { get; set; }
+}
