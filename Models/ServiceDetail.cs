793a3d41ea62a440c1b9d670d01ab9429867922b
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:28:43 +0200

    added serviceDetail model

---
+++ Models/ServiceDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace GaragePRO.Models;
+
+public class ServiceDetail
+{
+    public int Id { get; set; }
+
+    public int WorkOrderId { get; set; }
+    public WorkOrder WorkOrder { get; set; }
+
+    public string Description { get; set; }
+    public decimal LaborHours { get; set; }
+    public decimal HourlyRate { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
