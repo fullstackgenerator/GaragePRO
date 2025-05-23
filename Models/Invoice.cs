9e90cee25817a6909804cb216f23b4dcdf315e9c
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-23 22:31:46 +0200

    added Invoice model

---
+++ Models/Invoice.cs
@@ -0,0 +1,36 @@
+﻿namespace GaragePRO.Models;
+
+public class Invoice
+{
+    public int Id { get; set; }
+    public string InvoiceNumber { get; set; }
+
+    public int WorkOrderId { get; set; }
+    public WorkOrder WorkOrder { get; set; }
+
+    public decimal TaxAmount { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal Total { get; set; }
+    public decimal AmountDue { get; set; }
+    public decimal AmountPaid { get; set; }
+    public decimal AmountReturned { get; set; }
+
+    public PaymentType PaymentType { get; set; }
+    public InvoiceStatus Status { get; set; }
+
+    public DateTime DateIssued { get; set; }
+    public DateTime? DatePaid { get; set; }
+}
+
+public enum PaymentType
+{
+    Cash,
+    CreditCard,
+    DebitCard
+}
+
+public enum InvoiceStatus
+{
+    Paid,
+    Unpaid
+}
