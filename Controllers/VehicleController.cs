3251895de83454c2b0c63caedd76c7689081a602
fullstackgenerator <simon.hojnik@gmail.com>
2025-05-25 09:39:29 +0200

    updated vehicle controller, created create, detail, and edit vehicle views, added database

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -1,5 +1,7 @@
 ﻿using GaragePRO.Data;
+using GaragePRO.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GaragePRO.Controllers;
 
@@ -10,5 +12,55 @@ public class VehicleController : Controller
     public VehicleController(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+    public IActionResult Create(int customerId)
+    {
+        var vehicle = new Vehicle { customerId = customerId };
+        return View(vehicle);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,make,model,year,VIN,mileage,customerId")] Vehicle vehicle)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(vehicle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "Customer", new { id = vehicle.customerId });
+        }
+        return View(vehicle);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        if (id == null) return BadRequest();
+        var vehicle = await _context.Vehicles.FindAsync(id);
+        if (vehicle == null) return NotFound();
+        return View(vehicle);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,make,model,year,VIN,mileage,customerId")] Vehicle vehicle)
+    {
+        if (id != vehicle.Id) return BadRequest();
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(vehicle);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (vehicle == null) return NotFound();
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(vehicle);
     }
 }
